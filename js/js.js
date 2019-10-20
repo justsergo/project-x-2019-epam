12c0a0aef1fb8dfd5687285fb0cee4abc9b92a25
sergey_prosto_sergey <50928109+sergey6608@users.noreply.github.com>
2019-10-20 19:28:53 +0300

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,100 @@
+// page= index section= slider-carousel
+$('.slider-for').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    speed: 1000,
+    asNavFor: '.slider-nav'
+  });
+  $('.slider-nav').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    asNavFor: '.slider-for',
+    speed: 1200,
+    dots: false,
+    arrows: true,
+  });
+// page= index section= services
+  $('.slider-center').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    speed: 1000,
+    asNavFor: '.slider-control'
+  });
+  $('.slider-control').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    asNavFor: '.slider-center',
+    speed: 1200,
+    dots: false,
+    arrows: true,
+  });
+// page= index section= portfolio
+  $('.slider-bottom').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    speed: 1000,
+    asNavFor: '.slider-control-bottom'
+  });
+  $('.slider-control-bottom').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    asNavFor: '.slider-bottom',
+    speed: 1200,
+    dots: false,
+    arrows: true,
+  });
+// page= index section= information class= news
+  $('.slider-news').slick({
+    dots: true,
+    arrows: false,
+  });
+// page= index section= information class= review
+  $('.slider-review').slick({
+    dots: true,
+    arrows: false,
+  });
+// page= our-services section= services
+  $('.slider-posts').slick({
+    dots: true,
+    arrows: false,
+  });
+// page= post-page section= slider-post-page
+$('.slick-slider-post-page').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  fade: true,
+  speed: 100,
+  asNavFor: '.control-slider-post-page'
+});
+$('.control-slider-post-page').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  asNavFor: '.slick-slider-post-page',
+  speed: 120,
+  dots: false,
+  arrows: true,
+});
+// page= post-page section= slider-post-page
+$('.slides-section-contentpostpage').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  fade: true,
+  speed: 100,
+  asNavFor: '.controls-slider-left-right-post-page'
+});
+$('.controls-slider-left-right-post-page').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  asNavFor: '.slides-section-contentpostpage',
+  speed: 120,
+  dots: false,
+  arrows: true,
+});
